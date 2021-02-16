7e49bf25bbf853a2d33f65a0582aa11df3e53d2d
Thomas Broust <thomas.broust@gmail.com>
2021-02-16 15:09:10 +0100

    Fixes API call - now it returns an array

--- utils.js
+++
@@ -1,4 +0,0 @@
-function parseNintendo(url) {
-    // example url: https://www.nintendo.co.uk/Games/Nintendo-Switch-download-software/Heaven-s-Vault-1892235.html
-    return url.substring(url.lastIndexOf("-") + 1).replace(".html", "");
-}
